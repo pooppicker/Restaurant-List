2c817c9ac9efee7ccd6ccbe2e670feae3fe320c5
Wendy Tsai <>
2021-07-09 16:13:09 +0800

    feat: try to fix error

--- app.js
+++ app.js
@@ -19,20 +19,17 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
-// 建立名為 hbs 的樣版引擎
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
-// 啟用樣版引擎
 app.set('view engine', 'hbs')
-//靜態檔案放在public資料夾裡
 app.use(express.static('public'))
-//規定每一筆請求都需要透過body-parser進行前置處理
 app.use(express.urlencoded({ extended: true }))
 //設定路由
 app.get('/', (req, res) => {
-  Restaurant.find() //取出 Restaurant model 裡所有資料
-    .lean() //把 Mongoose 的 Model 物件轉換成乾淨的JS資料陣列
-    .then((restaurants) => res.render('index', { restaurants })) //將資料傳給index template
-    .catch(error => console.log(error)) //除錯
+  Restaurant.find() 
+    .lean() 
+    .then((restaurants) => res.render('index', { restaurants })) 
+    .catch(error => console.log(error)) 
 })
 
 app.get('/restaurants/new', (req, res) => {
@@ -49,17 +46,21 @@ app.post('/restaurants', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch((error) => console.error(error))
+})
+
 app.get('/restaurants/searches', (req, res) => {
   const keyword = req.query.keyword
   Restaurant.find()
     .lean()
     .then((restaurants) => {
       if (keyword) {
-        restaurants = restaurants.filter((restaurant) => 
-          restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
-        )
-        console.log('restaurants', restaurants)
-        console.log('restaurants.length', restaurants.length)
+        restaurants = restaurants.filter((restaurant) => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
       }
       if (restaurants.length === 0) {
         return res.render('index', { error: `<div class="alert alert-danger" role="alert">cannot find: "${keyword}", please search again!</div>` })
@@ -67,21 +68,10 @@ app.get('/restaurants/searches', (req, res) => {
       res.render('index', { restaurants, keyword })
     })
     .catch(error => console.log(error))
-  
-})
-
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch((error) => console.error(error))
 })
 
 app.get('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
   return Restaurant.findById(id)
     .lean()
     .then((restaurant) => res.render('edit', { restaurant }))
@@ -90,7 +80,6 @@ app.get('/restaurants/:id/edit', (req, res) => {
 
 app.post('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
-  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
   const { name, name_en, category, location, phone, rating, google_map, image, description } = req.body
   return Restaurant.findById(id)
     .then(restaurant => {
@@ -105,8 +94,9 @@ app.post('/restaurants/:id/edit', (req, res) => {
       restaurant.description = description
       return restaurant.save()
     })
-    .then(()=> res.redirect(`/restaurants/${id}`))
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
+
 })
 
 app.listen(port, () => {
